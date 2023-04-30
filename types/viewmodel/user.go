ea1e7d81c064a8993a51cb6a04ab32aa51929346
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-04-30 19:08:58 -0300

    added name validation

--- types/viewmodel/user.go
+++ types/viewmodel/user.go
@@ -12,13 +12,16 @@ type RegisterUser struct {
 
 func (r *RegisterUser) Validate() (e []string) {
 	if r.Login == "" {
-		e = append(e, "empty Login")
+		e = append(e, "empty `login`")
 	}
 	if match, _ := regexp.MatchString(`^[a-z0-9]+(?:(?:-|_)+[a-z0-9]+)*$`, r.Login); !match {
 		e = append(e, "login must not contain spaces or special characters")
 	}
 	if r.Password == "" {
-		e = append(e, "empty Password")
+		e = append(e, "empty `password`")
+	}
+	if r.Name == "" {
+		e = append(e, "empty `name`")
 	}
 
 	return e
